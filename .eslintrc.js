8a8c16af8ffd092c949c78e55c43f31bf6293d19
Ranisans <ranisans@yahoo.com>
2022-01-26 12:31:31 +0300

    init: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+    'no-restricted-syntax': ['off', 'ForInStatement'],
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
